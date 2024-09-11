9829439c8292ab700d7bc0ce6ef937a7817dcbd9
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-09-11 11:09:13 -0500

    Maven proyect

--- P2PProject/src/p2p/core/P2PNetwork.java
+++
@@ -1,8 +0,0 @@
-package p2p.core;
-
-public class P2PNetwork {
-    public static void main(String[] args) {
-        AdminPanel adminPanel = new AdminPanel();
-        adminPanel.start();
-    }
-}
